333df8fbe8e03b1a57d645c0a339cfcdaeac850c
Andrew Jeddeloh <andrew.jeddeloh@gmail.com>
2020-05-05 17:05:38 -0700

    lrc: use linear regression to get L

--- lrc.py
+++ lrc.py
@@ -7,13 +7,13 @@ from scope import Rigol1054z
 chanSettingsFile = "chan.txt"
 scopeSettingsFile = "scope.txt"
 
-if len(sys.argv) != 4:
-    print("usage: lrc <start freq> <stop freq> <freq step>")
+if len(sys.argv) != 5:
+    print("usage: lrc <start freq> <stop freq> <freq step> <r>")
 
 startFreq = int(sys.argv[1])
 stopFreq = int(sys.argv[2])
 freqStep = int(sys.argv[3])
-resistor = 99.4
+resistor = float(sys.argv[4])
 
 with open(chanSettingsFile) as f:
     chanSettings = f.readlines()
@@ -23,11 +23,12 @@ with open(scopeSettingsFile) as f:
 
 scope = Rigol1054z()
 scope.w(*scopeSettings)
-time.sleep(1)
-tdiv = scope.qf("wav:xinc?")
 scope.w(*["chan1:%s" % setting for setting in chanSettings])
 scope.w(*["chan2:%s" % setting for setting in chanSettings])
-scope.w("chan2:scale 0.001")
+scope.w("chan2:scale 0.005", timeout=.5)
+scope.w("sing", "tfor", timeout=.5)
+time.sleep(1)
+tdiv = scope.qf("wav:xinc?")
 
 # connect to fn generator
 ser = serial.Serial("/dev/ttyUSB0", 115200)
@@ -45,39 +46,40 @@ def setchan2(scope):
     while max(ch2) > 245 or min(ch2) < 15:
         scale = scope.qf("chan2:scale?")
         scope.w(("chan2:scale %f" % (scale*2)),
-                "sing", "tfor", timeout=0.1)
+                "sing", "tfor", timeout=0.2)
         ch2 = scope.grab_all_raw(2)
-    print(max(ch2), min(ch2))
+    return ch2
 
 def measure(freq):
-    scope.w("sing", "tfor", timeout=0.1)
-    ch1, ch2 = scope.grab_all(1), scope.grab_all(2)
+    ch2 = setchan2(scope)
+    ch1 = scope.grab_all(1)
     fft1, fft2 = fft.rfft(ch1[1]), fft.rfft(ch2[1])
     n_samples = len(ch1[1])
     bin_size = 1/(n_samples*tdiv)
     bin = int(np.round(freq/bin_size))
-    print(bin, bin_size, n_samples)
+    print(bin)
     vL = fft.rfft(ch2[1])[bin]
     vT = fft.rfft(ch1[1])[bin]
-    return getZLR(freq, vT, vL, resistor)
+    return getZ(freq, vT, vL, resistor)
 
-def getZLR(freq, vtotal, vind, r):
+def getZ(freq, vtotal, vind, r):
     k = vind/vtotal
     z = (-k*r)/(k-1)
-    return z, 1e6*np.imag(z)/(2*np.pi*freq), np.real(z)
+    print(z)
+    return z
 
-data = []
+freqs, zs = [], []
 for freq in range(startFreq, stopFreq+1, freqStep):
     # Frequency is set as an integer number of uHz
     fngen(b'WMF%d'% (freq*1e6))
     time.sleep(.1)
-    setchan2(scope)
-    point = [measure(freq)[1] for x in range(0,5)]
-    data.append([freq, point])
+    zs.append(measure(freq))
+    freqs.append(freq)
 
-ser.close()
-transposed = list(zip(*data))
-plt.plot(transposed[0], transposed[1])
+res = np.polyfit(freqs, np.array(zs), 1)
+print(res[1], res[0]/(1j*2*np.pi))
+plt.plot(freqs, np.imag(np.array(zs)))
+plt.plot(freqs, np.imag(res[1]+res[0]*np.array(freqs)))
 
 plt.show()
 
